d25d0efccf80b83e0d988ef0c67fa031d6dfc81a
GeertJohan <gjr19912@gmail.com>
2013-01-20 12:43:02 +0100

    Improved http request dispatching/routing.

--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strings"
 )
 
-var tmplSimplePage *template.Template
+var (
+	tmplSimplePage *template.Template
+	tmplGoGet      *template.Template
+)
 
-type dataSimplePage struct {
+type dataTmplSimplePage struct {
 	Identifier       string
 	OriginalRepoPath string
 	RefType          string
 	Ref              string
+}
+
+type dataTmplGoGet struct {
+	Identifier string
 }
 
 func init() {
@@ -22,7 +30,6 @@ func init() {
 <html>
 	<head>
 		<meta charset="utf-8">
-		<meta name="go-import" content="localhost.com/{{.Identifier}} git http://localhost.com/{{.Identifier}}/.git/">
 		<title>RepoRef: {{.Identifier}}</title>
 	</head>
 	<body>
@@ -34,54 +41,94 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+
+	tmplGoGet, err = template.New("goGet").Parse(`
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="go-import" content="go.reporef.org/{{.Identifier}} git http://go.reporef.org/{{.Identifier}}/.git/">
+		<meta HTTP-EQUIV="REFRESH" content="0; url=http://go.reporef.org/{{.Identifier}}">
+		<title>RepoRef go-get: {{.Identifier}}</title>
+	</head>
+	<body>
+		This is the go-get page for go.reporef.org/{{.Identifier}}
+		You are being redirected to the human-readable page.
+	</body>
+</html>
+`)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
 
-	// serve requests for the /git dir
+	// serve requests for /.git/ repo's
 	gitHttpHandler := http.FileServer(http.Dir(localDataPath))
-	// http.HandleFunc("/git/", func(w http.ResponseWriter, r *http.Request) {
-	// 	log.Printf("/git/ request: %s\n", r.RequestURI)
-	// 	gitHttpHandler.ServeHTTP(w, r)
-	// })
 
-	// serve any request in the root (serve website with meta tag for go-get, redirecting to /git/)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		if r.RequestURI == "/" {
-			http.Redirect(w, r, "https://github.com/GeertJohan/reporef", 307)
-		}
-		log.Println("request on", r.RequestURI)
+	statsHandler := func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Not available yet.")
+	}
+
+	reporefHandler := func(w http.ResponseWriter, r *http.Request) {
+		// Get the rr for this request
 		rr, err := reporefFromRequestURI(r.RequestURI)
 		if err != nil {
 			log.Printf("Error: %s\n", err)
 			return
 		}
 
-		done, err := rr.updateRepositoryIfNeeded()
+		// Update if required.
+		// TODO: concurrent update process
+		_, err = rr.updateRepositoryIfNeeded()
 		if err != nil {
 			log.Printf("Error: %s\n", err)
 			return
 		}
-		if done {
-			log.Println("updated!")
-		} else {
-			log.Println("not updated")
-		}
 
+		// Request is for the .git repository.
 		if strings.HasPrefix(r.RequestURI, "/"+rr.identifier+"/.git/") {
 			gitHttpHandler.ServeHTTP(w, r)
 			return
 		}
 
-		pageData := &dataSimplePage{
+		// go-get page with meta tag
+		if r.FormValue("go-get") == "1" {
+			pageData := &dataTmplGoGet{rr.identifier}
+			tmplGoGet.Execute(w, pageData)
+			return
+		}
+
+		// Information page
+		pageData := &dataTmplSimplePage{
 			Identifier:       rr.identifier,
 			OriginalRepoPath: rr.originalRepoPath,
 			RefType:          string(rr.refType),
 			Ref:              rr.ref,
 		}
-
 		tmplSimplePage.Execute(w, pageData)
-	})
+	}
+
+	// Manual mux for all requests
+	rootHandler := func(w http.ResponseWriter, r *http.Request) {
+		if r.RequestURI == "/" {
+			// There's no index or home page yet. Redirect to reporef github project.
+			http.Redirect(w, r, "https://github.com/GeertJohan/reporef", 307)
+		}
+
+		fields := strings.SplitN(r.RequestURI[1:], "/", 1)
+		switch fields[0] {
+		case "stats":
+			statsHandler(w, r)
+		case "github.com":
+			reporefHandler(w, r)
+		default:
+			http.NotFound(w, r)
+		}
+	}
+
+	// serve any request in the root (serve website with meta tag for go-get, redirecting to /git/)
+	http.HandleFunc("/", rootHandler)
 
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
